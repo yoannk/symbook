388f74c3051473539b7e76c51d2ff1b3614f2866
Nkoyan <yoann.kergall@gmail.com>
2019-04-24 00:10:02 +0100

    create form to add Books

--- src/Controller/BookController.php
+++ src/Controller/BookController.php
@@ -3,25 +3,49 @@
 namespace App\Controller;
 
 use App\Entity\Book;
+use App\Form\BookType;
 use App\Repository\BookRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * @Route("/")
- */
 class BookController extends AbstractController
 {
     /**
-     * @Route("/new", name="book_new", methods={"GET", "POST"})
+     * @Route("/", name="book_index")
      */
-    public function new()
+    public function index(BookRepository $bookRepository)
     {
-
+        return $this->render('book/index.html.twig', [
+            'books' => $bookRepository->findBy([], ['id' => 'DESC'])
+        ]);
     }
 
     /**
-     * @Route("/{id}", name="book_show", methods={"GET"})
+     * @Route("/new", name="book_new", methods={"GET", "POST"})
+     */
+    public function new(Request $request)
+    {
+        $book = new Book();
+        $form = $this->createForm(BookType::class, $book);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($book);
+            $em->flush();
+
+            $this->addFlash('success', 'Livre ajouté avec succès !');
+            $this->redirectToRoute('book_index');
+        }
+
+        return $this->render('book/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/{id<[1-9]\d*>}", name="book_show", methods={"GET"})
      */
     public function show(Book $book)
     {
@@ -39,10 +63,18 @@ class BookController extends AbstractController
     }
 
     /**
-     * @Route("/{id}", name="book_delete", methods={"DELETE"})
+     * @Route("/{id}/delete", name="book_delete", methods={"GET"})
      */
-    public function delete(Book $book)
+    public function delete(Book $book, Request $request)
     {
+        if ($this->isCsrfTokenValid('delete'.$book->getId(), $request->query->get('token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($book);
+            $entityManager->flush();
 
+            $this->addFlash('success', 'Le livre a bien été supprimé');
+        }
+
+        return $this->redirectToRoute('book_index');
     }
 }
--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\BookRepository")
@@ -18,11 +19,18 @@ class Book
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Length(
+     *     min=10,
+     *     max=100,
+     *     minMessage="Le titre doit faire au moins {{ limit }} caractères",
+     *     maxMessage="Le titre ne doit pas dépasser {{ limit }} caractères"
+     * )
      */
     private $title;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\Length(min=15, minMessage="La description doit faire au moins {{ limit }} caractères")
      */
     private $description;
 
