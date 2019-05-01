47671e24c72b7fb51f69ef7585a8b38368acd80d
Nkoyan <yoann.kergall@gmail.com>
2019-05-01 18:29:37 +0100

    add maintenance mode

---
+++ src/EventSubscriber/MaintenanceSubscriber.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Twig\Environment;
+
+class MaintenanceSubscriber implements EventSubscriberInterface
+{
+    private $twig;
+    private $maintenanceMode;
+
+    public function __construct(Environment $twig, $maintenanceMode)
+    {
+        $this->twig = $twig;
+        $this->maintenanceMode = $maintenanceMode;
+    }
+
+    public function onKernelRequest(GetResponseEvent $event)
+    {
+        if ($this->maintenanceMode && $event->getRequest()->attributes->get('_route') === 'book_index') {
+            $content = $this->twig->render('maintenance/maintenance.html.twig');
+            $response = new Response($content);
+
+            $event->setResponse($response);
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+           'kernel.request' => 'onKernelRequest',
+        ];
+    }
+}
